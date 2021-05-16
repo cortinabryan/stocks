496862259888fb0615b539705d41024f48007844
Bryan Cortina <cortinabryan@gmail.com>
2021-05-16 11:34:07 -0700

    learning how to add stocks api. taking skillshare course

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+const otherstuff = "hello there, this is other stuff!";
+const testlol = "hello this is test :D";
+
+// Set Handlebar routes
+app.get("/", function (req, res) {
+  res.render("home", {
+    stuff: otherstuff,
+    test: testlol,
+  });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log("Server Listening on port " + PORT));
